4349a895414238290eeb1ed1fb01e48fbc1f9220
Samuel Pagenkopf <ssaammp@gmail.com>
2018-03-15 03:44:15 -0500

    cargo

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod emu;
+
+fn main() {
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut bin = vec![];
+    File::open("test.bin").unwrap().
+        read_to_end(&mut bin).unwrap();
+
+    let rom = emu::Rom {
+        rom: bin,
+        offset: 0x8000,
+    };
+
+    emu::Chip::run(rom);
+}
