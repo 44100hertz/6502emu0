7527c2da0d3a641488240e11c4dbbc2d4c4f89d4
Samuel Pagenkopf <ssaammp@gmail.com>
2018-03-15 22:27:40 -0500

    jmp and some others

--- test.asm
+++ test.asm
@@ -1,16 +1,20 @@
-.org $8000
+org $8000
 
 REG_PUTC = $6000
 
 text:
   .db 'hello, world', $0a, 0
 main:
-  ldx #$ff
-print_hello:
+  ldx #0
+  jmp readchar
+putchar:
   inx
-  lda text, x
   sta REG_PUTC
-  bne print_hello
+readchar:
+  lda text, x
+  bne putchar
+
+  brk
 
 .org $fffa
   dw main
