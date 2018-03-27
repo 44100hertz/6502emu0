66b846d2d69fd3ecbb4c6eae4203a1b5f3ef9966
Samuel Pagenkopf <ssaammp@gmail.com>
2018-03-26 21:14:09 -0500

    value-writing macro and other asm fixes

--- test.asm
+++ test.asm
@@ -3,18 +3,18 @@
 	.cdef $0,$7f,$0
 	.edef "\n", $0a
 
-reg_putc	:= $6000
-reg_putnum	:= $6001
+reg_putc	= $6000
+reg_putnum	= $6001
 
-string_ptr	:= $0
-mult0		:= $10
-mult1		:= $11
+string_ptr	= $0
+mult0		= $10
+mult1		= $11
 
 main
-	.mul_test $00, $00
-	.mul_test $04, $04
-	.mul_test $10, $10
-	.mul_test $ff, $ff
+	#mul_test $00, $00
+	#mul_test $04, $04
+	#mul_test $10, $10
+	#mul_test $ff, $ff
 	brk
 
 put16	.macro val, into
@@ -22,7 +22,18 @@ put16	.macro val, into
 	sta \into
 	lda >\val
 	sta \into+1
-	.endm
+.endm
+
+writev	.macro
+vals	= (\@)
+	.cerror len(vals) % 2 == 1
+	.for i := 0, i < len(vals), i += 2
+		.if i < 2 || vals[i-2] != vals[i]
+			lda vals[i]
+		.endif
+		sta vals[i+1]
+	.next
+.endm
 
 mul_test .macro v0, v1
 	jmp start
@@ -32,19 +43,12 @@ start
 	.put16 #info, string_ptr
 	jsr print_string
 	;; multiply
-	lda #\v0
-	sta mult0
-	lda #\v1
-	sta mult1
+	#writev #\v0, mult0, #\v1, mult1
 	jsr mult
-	;; print result
-	lda mult1
-	sta reg_putnum
-	lda mult0
-	sta reg_putnum
-	lda #"\n"
-	sta reg_putc
-	.endm
+	;; print result and newline
+	#writev mult1, reg_putnum, mult0, reg_putnum
+	#writev #10, reg_putc
+.endm
 
 mult
 	lda #0
