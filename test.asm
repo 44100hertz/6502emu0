c47b89b241e0c3bba3bed282e4c41c752199243d
Samuel Pagenkopf <ssaammp@gmail.com>
2018-03-26 23:07:29 -0500

    use a macro rather than subroutine for string print

--- test.asm
+++ test.asm
@@ -6,7 +6,6 @@
 reg_putc	= $6000
 reg_putnum	= $6001
 
-string_ptr	= $0
 mult0		= $10
 mult1		= $11
 
@@ -17,40 +16,43 @@ main
 	#mul_test $ff, $ff
 	brk
 
-put16	.macro val, into
-	lda <\val
-	sta \into
-	lda >\val
-	sta \into+1
-.endm
-
 writev	.macro
-vals	= (\@)
-	.cerror len(vals) % 2 == 1
+	vals = [\@]
 	.for i := 0, i < len(vals), i += 2
 		.if i < 2 || vals[i-2] != vals[i]
 			lda vals[i]
 		.endif
 		sta vals[i+1]
 	.next
-.endm
+	.endm
+
+puts	.macro s
+	strlen = len(\s)
+	.cerror strlen > $ff, "string too large"
+	.if strlen > 0
+	;; start x at a higher place so that it wraps to 0, eliminating a
+	;; comparison
+	x_start = $100 - strlen
+	ldx #x_start
+	bne _print
+string	.text \s
+_print
+	lda string - x_start, x
+	sta reg_putc
+	inx
+	bne _print
+	.endif
+	.endm
 
 mul_test .macro v0, v1
-	jmp start
-	;; info
-info	.null format("multiply %02x and %02x, expects %04x: ", \v0, \v1, \v0*\v1)
-start
-	.put16 #info, string_ptr
-	jsr print_string
-	;; multiply
+	.puts format("multiply %02x and %02x, expects %04x: ", \v0, \v1, \v0*\v1)
 	#writev #\v0, mult0, #\v1, mult1
 	jsr mult
-	;; print result and newline
 	#writev mult1, reg_putnum, mult0, reg_putnum
-	#writev #10, reg_putc
-.endm
+	#writev #"\n", reg_putc
+	.endm
 
-mult
+mult	.proc
 	lda #0
 	ldx #8
 	lsr mult0
@@ -66,18 +68,7 @@ _noadd
 	sta mult1
 
 	rts
-
-print_string
-	ldy #0
-_putc
- 	lda (string_ptr),y
-	beq _done
-	sta reg_putc
-	iny
-	beq _done
-	bne _putc
-_done
-	rts
+	.pend
 
 	* = $fffa
 	.addr main, 0, 0
